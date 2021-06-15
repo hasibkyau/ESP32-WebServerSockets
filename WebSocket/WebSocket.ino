1bb4d023b2f03126ec073fa589c23e4557df4810
Hasib R Rahman <hasibkyau.cse@gmail.com>
2021-06-15 19:31:02 +0600

    it can send message from server

---
+++ WebSocket/WebSocket.ino
@@ -0,0 +1,158 @@
+#include <WiFi.h>
+#include <SPIFFS.h>
+#include <ESPAsyncWebServer.h>
+#include <WebSocketsServer.h>
+
+// Constants
+const char *ssid = "ESP32-AP";
+const char *password =  "LetMeInPlz";
+const char *msg_toggle_led = "toggleLED";
+const char *msg_get_led = "getLEDState";
+const int dns_port = 53;
+const int http_port = 80;
+const int ws_port = 1337;
+const int led_pin = 15;
+
+// Globals
+AsyncWebServer server(80);
+WebSocketsServer webSocket = WebSocketsServer(1337);
+char msg_buf[10];
+int led_state = 0;
+
+/***********************************************************
+ * Functions
+ */
+
+// Callback: receiving any WebSocket message
+void onWebSocketEvent(uint8_t client_num,
+                      WStype_t type,
+                      uint8_t * payload,
+                      size_t length) {
+
+  // Figure out the type of WebSocket event
+  switch(type) {
+
+    // Client has disconnected
+    case WStype_DISCONNECTED:
+      Serial.printf("[%u] Disconnected!\n", client_num);
+      break;
+
+    // New client has connected
+    case WStype_CONNECTED:
+      {
+        IPAddress ip = webSocket.remoteIP(client_num);
+        Serial.printf("[%u] Connection from ", client_num);
+        Serial.println(ip.toString());
+      }
+      break;
+
+    // Handle text messages from client
+    case WStype_TEXT:
+
+      // Print out raw message
+      Serial.printf("[%u] Received text: %s\n", client_num, payload);
+
+      // Toggle LED
+      if ( strcmp((char *)payload, "toggleLED") == 0 ) {
+        led_state = led_state ? 0 : 1;
+        Serial.printf("Toggling LED to %u\n", led_state);
+        digitalWrite(led_pin, led_state);
+
+      // Report the state of the LED
+      } else if ( strcmp((char *)payload, "getLEDState") == 0 ) {
+        sprintf(msg_buf, "%d", led_state);
+        Serial.printf("Sending to [%u]: %s\n", client_num, msg_buf);
+        webSocket.sendTXT(client_num, msg_buf);
+
+      // Message not recognized
+      } else {
+        Serial.println("[%u] Message not recognized");
+      }
+      break;
+
+    // For everything else: do nothing
+    case WStype_BIN:
+    case WStype_ERROR:
+    case WStype_FRAGMENT_TEXT_START:
+    case WStype_FRAGMENT_BIN_START:
+    case WStype_FRAGMENT:
+    case WStype_FRAGMENT_FIN:
+    default:
+      break;
+  }
+}
+
+// Callback: send homepage
+void onIndexRequest(AsyncWebServerRequest *request) {
+  IPAddress remote_ip = request->client()->remoteIP();
+  Serial.println("[" + remote_ip.toString() +
+                  "] HTTP GET request of " + request->url());
+  request->send(SPIFFS, "/index.html", "text/html");
+}
+
+// Callback: send style sheet
+void onCSSRequest(AsyncWebServerRequest *request) {
+  IPAddress remote_ip = request->client()->remoteIP();
+  Serial.println("[" + remote_ip.toString() +
+                  "] HTTP GET request of " + request->url());
+  request->send(SPIFFS, "/style.css", "text/css");
+}
+
+// Callback: send 404 if requested file does not exist
+void onPageNotFound(AsyncWebServerRequest *request) {
+  IPAddress remote_ip = request->client()->remoteIP();
+  Serial.println("[" + remote_ip.toString() +
+                  "] HTTP GET request of " + request->url());
+  request->send(404, "text/plain", "Not found");
+}
+
+/***********************************************************
+ * Main
+ */
+
+void setup() {
+  // Init LED and turn off
+  pinMode(led_pin, OUTPUT);
+  digitalWrite(led_pin, LOW);
+
+  // Start Serial port
+  Serial.begin(115200);
+
+  // Make sure we can read the file system
+  if( !SPIFFS.begin()){
+    Serial.println("Error mounting SPIFFS");
+    while(1);
+  }
+
+  // Start access point
+  WiFi.softAP(ssid, password);
+
+  // Print our IP address
+  Serial.println();
+  Serial.println("AP running");
+  Serial.print("My IP address: ");
+  Serial.println(WiFi.softAPIP());
+
+  // On HTTP request for root, provide index.html file
+  server.on("/", HTTP_GET, onIndexRequest);
+
+  // On HTTP request for style sheet, provide style.css
+  server.on("/style.css", HTTP_GET, onCSSRequest);
+
+  // Handle requests for pages that do not exist
+  server.onNotFound(onPageNotFound);
+
+  // Start web server
+  server.begin();
+
+  // Start WebSocket server and assign callback
+  webSocket.begin();
+  webSocket.onEvent(onWebSocketEvent);
+  
+}
+
+void loop() {
+  
+  // Look for and handle WebSocket data
+  webSocket.loop();
+}
